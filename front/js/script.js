39f7ff543280b57043c8dfa1dbc4cb29074f8637
lucascardelima <lucas@marchettidelima.com.br>
2022-03-15 20:59:34 +0000

    implement the GET request and the Asyn/await func

--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,18 @@
 // Get acess to the DOM Elements from the index page
 
+const items = document.getElementById('items');
+const itemPhoto = document.querySelector('img.itemPhoto');
+const productName = document.querySelector('h3.productName');
+const productDescription = document.querySelector('p.productDescription');
+
+
 // Make a GET request
 
 function makeRequest(verb, url) {
-    return new Promisse ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         let request = new XMLHttpRequest();
-        request.open(verb, url);
         
-        request.onsteadystatechange = () => {
+        request.onreadystatechange = () => {
             if (request.readyState === 4) {
                 if (request.status === 200) {
                     resolve(JSON.parse(request.response));
@@ -16,6 +21,39 @@ function makeRequest(verb, url) {
                 }
             }
         }
+        
+        request.open(verb, url);
+        request.send();
     });
 };
+
+
+
+async function getProducts() {
+    const requestPromise = makeRequest("GET", "http://localhost:3000/api/products");
+    const products = await requestPromise;
+
+    const primeiro = products[0];
+    
+    itemPhoto.src = primeiro.imageUrl;
+    productName.textContent = primeiro.name;
+    productDescription.textContent = primeiro.description;
+
+};
+
+getProducts();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
